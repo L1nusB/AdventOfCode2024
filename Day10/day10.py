d7b8149bdc26ddb94a15864cbefdde07243a7f15
Linus <linus@beckhaus.com>
2025-03-31 21:21:12 +0200

    Finish Day10

--- Day10/day10.py
+++ Day10/day10.py
@@ -22,27 +22,38 @@ def get_trailhead_starts(top_map: np.ndarray) -> np.ndarray:
     return np.asarray(np.nonzero(top_map == 0)).T
 
 
-def traverse(top_map: np.ndarray, cur_pos: Tuple[int, int], cur_height: int, visited: Set[Tuple[int, int]], target_height: int = 9) -> Tuple[int, Set[Tuple[int, int]]]:
-    """Uses stateful DFS (depth-first search) which keeps track of all visited fields to avoid loops and revisiting"""
-    # Check if already visited current position
-    if cur_pos in visited:
-        return 0, visited
+def traverse(top_map: np.ndarray,
+             cur_pos: Tuple[int, int],
+             cur_height: int,
+             visited: Set[Tuple[int, int]],
+             target_height: int = 9,
+             allow_revists: bool = False
+             ) -> Tuple[int, Set[Tuple[int, int]]]:
+    """Uses stateful or stateless DFS (depth-first search).
+    In stateful mode keeps track of all visited fields to avoid loops and revisiting i.e. only checking if a path is a valid trailhead.
+    In stateless count all distinct trails."""
+    if not allow_revists:
+        # Check if already visited current position
+        if cur_pos in visited:
+            return 0, visited
     visited.add(cur_pos)
-    
+
     # If target height/peak has been reached register as valid trailhead
     if cur_height >= target_height:
         return 1, visited
 
-    next_steps = [(cur_pos[0]+1, cur_pos[1]), # Move down
-                  (cur_pos[0]-1, cur_pos[1]), # Move up
-                  (cur_pos[0], cur_pos[1]+1), # Move right
-                  (cur_pos[0], cur_pos[1]-1)] # Move left
+    next_steps = [(cur_pos[0]+1, cur_pos[1]),  # Move down
+                  (cur_pos[0]-1, cur_pos[1]),  # Move up
+                  (cur_pos[0], cur_pos[1]+1),  # Move right
+                  (cur_pos[0], cur_pos[1]-1)]  # Move left
     # Filter steps outside the boundaries
-    next_steps = [(i,j) for i,j in next_steps if ((i>=0 and i<top_map.shape[0]) and (j>=0 and j<top_map.shape[1]))]
+    next_steps = [(i, j) for i, j in next_steps if (
+        (i >= 0 and i < top_map.shape[0]) and (j >= 0 and j < top_map.shape[1]))]
     valid_heads = 0
     for step in next_steps:
         if top_map[*step] == cur_height+1:
-            valid_sub_heads, visited = traverse(top_map, step, cur_height+1, visited, target_height)
+            valid_sub_heads, visited = traverse(
+                top_map, step, cur_height+1, visited, target_height, allow_revists)
             valid_heads += valid_sub_heads
     return valid_heads, visited
 
@@ -53,9 +64,19 @@ if __name__ == "__main__":
     # top_map = parse_input("Day10/test3.txt")
     # top_map = parse_input("Day10/test4.txt")
     # top_map = parse_input("Day10/test5.txt")
+    # top_map = parse_input("Day10/input.txt")
+    # trailheads = get_trailhead_starts(top_map)
+    # trailhead_values = [traverse(top_map, tuple(start), 0, set(), 9)[
+    #     0] for start in trailheads]
+    # score = sum(trailhead_values)
+    # print(score)
+    ### Finish Part 1 ###
+    # top_map = parse_input("Day10/test6.txt")
+    # top_map = parse_input("Day10/test7.txt")
     top_map = parse_input("Day10/input.txt")
     trailheads = get_trailhead_starts(top_map)
-    trailhead_values = [traverse(top_map, tuple(start), 0, set(), 9)[0] for start in trailheads]
+    trailhead_values = [traverse(top_map, tuple(start), 0, set(), 9, True)[
+        0] for start in trailheads]
     score = sum(trailhead_values)
     print(score)
     a = 0
